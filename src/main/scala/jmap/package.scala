60e3b5805fc8f02d7c2dab99639aa5632ce030c9
Rémi KOWALSKI <rkowalski@linagora.com>
2020-11-05 16:13:27 +0100

    fixup! wip serialization jmap invocation list

--- src/main/scala/jmap/package.scala
+++ src/main/scala/jmap/package.scala
@@ -2,24 +2,6 @@ import zio.json.ast.Json
 import zio.json.{DeriveJsonDecoder, JsonDecoder}
 import zio.json.ast.Json.Obj
 
-/** **************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one   *
- * or more contributor license agreements.  See the NOTICE file *
- * distributed with this work for additional information        *
- * regarding copyright ownership.  The ASF licenses this file   *
- * to you under the Apache License, Version 2.0 (the            *
- * "License"); you may not use this file except in compliance   *
- * with the License.  You may obtain a copy of the License at   *
- * *
- * http://www.apache.org/licenses/LICENSE-2.0                 *
- * *
- * Unless required by applicable law or agreed to in writing,   *
- * software distributed under the License is distributed on an  *
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
- * KIND, either express or implied.  See the License for the    *
- * specific language governing permissions and limitations      *
- * under the License.                                           *
- * ************************************************************** */
 package object jmap {
 
   sealed trait Invocation
@@ -28,19 +10,13 @@ package object jmap {
 
   object Invocation {
     implicit val decoder: JsonDecoder[Invocation] =
-      JsonDecoder[(MethodName, Obj, ClientId)].map {
-        case (MethodName.mailboxGet, arguments, clientId) => MailboxGet(MethodName.mailboxGet,
-          //TODO unsafe and ugly
-          MailboxGetResponse.decoder.decodeJson(Json.encoder.encodeJson(arguments, None)).right.get,
-          clientId)
-        case (MethodName.mailboxQuery, arguments, clientId) => MailboxQuery(MethodName.mailboxQuery,
-          //TODO unsafe and ugly
-          MailboxQueryResponse.decoder.decodeJson(Json.encoder.encodeJson(arguments, None)).right.get,
-          clientId)
-
+      JsonDecoder[(MethodName, Obj, ClientId)].mapOrFail {
+        case (MethodName.mailboxGet, arguments, clientId) => MailboxGetResponse.decoder.decodeJson(Json.encoder.encodeJson(arguments, None))
+          .map( MailboxGet(MethodName.mailboxGet, _, clientId))
+        case (MethodName.mailboxQuery, arguments, clientId) => MailboxQueryResponse.decoder.decodeJson(Json.encoder.encodeJson(arguments, None))
+          .map( MailboxQuery(MethodName.mailboxQuery, _, clientId))
       }
   }
-
 
   case class JmapResponse(methodResponses: List[Invocation], sessionState: SessionState)
   object JmapResponse {
